d06401cb7816297280167f3a1453d1c6eefeb1e3
hendrafebrian10 <hendrafebrian10@gmail.com>
2022-08-06 13:22:03 +0700

    add: First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container py-4">
+        <h1>React Table Tutorial</h1>
+        <h4>Create table reusable for react aplication</h4>
+        <button className="btn btn-danger">
+            <em className="fas fa-trash"></em>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
